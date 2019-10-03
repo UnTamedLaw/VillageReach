098dfa7c45d460495e99d1c7e77eec3fe444deed
chinjaorosu <onetowthinkbule@gmail.com>
2019-10-03 13:28:32 -0700

    added DeliveryActivity and FormActivity

---
+++ app/src/main/java/com/lawk/villagereach/FormActivity.java
@@ -0,0 +1,13 @@
+package com.lawk.villagereach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
