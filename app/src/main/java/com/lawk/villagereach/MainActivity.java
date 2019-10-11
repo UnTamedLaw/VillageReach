fc63c1ab3f357e974cc778365d6c2494809d10ce
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-10-10 18:25:43 -0700

    dev2 start

--- app/src/main/java/com/lawk/villagereach/MainActivity.java
+++ app/src/main/java/com/lawk/villagereach/MainActivity.java
@@ -1,88 +1,14 @@
 package com.lawk.villagereach;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.widget.Toolbar;
+public class MainActivity extends AppCompatActivity {
 
-
-
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    private Toolbar topToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        topToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(topToolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggleIcon = new ActionBarDrawerToggle(this,
-                drawer,
-                topToolbar,
-                R.string.nav_open_drawer,
-                R.string.nav_close_drawer);
-        drawer.addDrawerListener(toggleIcon);
-        toggleIcon.syncState();
-
-        Fragment fragment = new DeliveryFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.content_fragments, fragment);
-        transaction.commit();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        Fragment fragment = null;
-        Intent intent = null;
-
-        switch(id) {
-            case R.id.nav_delivery:
-                fragment = new DeliveryFragment();
-                break;
-            case R.id.nav_sync:
-                fragment = new SyncFragment();
-                break;
-            default:
-                intent = new Intent(this, DeliveryActivity.class);
-        }
-        if (fragment != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.content_fragments, fragment);
-            ft.commit();
-        } else {
-            startActivity(intent);
-        }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        setContentView(R.layout.activity_delivery);
     }
 }
