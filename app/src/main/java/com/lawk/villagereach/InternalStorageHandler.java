f6ad0d0702f8ad207f3571becb29aae5195949a7
gillilands19 <gillilands19@gmail.com>
2019-10-14 19:55:22 -0700

    Begins construction of InternalStorageClass

---
+++ app/src/main/java/com/lawk/villagereach/InternalStorageHandler.java
@@ -0,0 +1,68 @@
+package com.lawk.villagereach;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
+
+public class InternalStorageHandler {
+
+    private String fileString;
+    private Context context;
+    private String fileName;
+    private JSONObject jsonObject;
+
+    public InternalStorageHandler(Context context, String fileName) {
+        this.context = context;
+        this.fileName = fileName;
+    }
+
+    public String readFile(Context context, String fileName) {
+        try {
+            FileInputStream fileInputStream = context.openFileInput(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        } catch (FileNotFoundException e) {
+            Log.e("Read File", "File Not Found" + e.toString());
+        } catch (IOException e) {
+            Log.e("Read File", "File Read Failed" + e.toString());
+        }
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(this.fileName, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+
+    JSONObject fileStringToJSONObject(String fileString) throws JSONException {
+        return new JSONObject(fileString);
+    }
+
+
+}
