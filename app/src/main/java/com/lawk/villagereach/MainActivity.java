530ebe84360570d45a9dd0ab6e70e2507b4e2d7c
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-10-16 17:43:21 -0700

    Added new Login class that will contain the username and password separate from Main Activity

--- app/src/main/java/com/lawk/villagereach/MainActivity.java
+++ app/src/main/java/com/lawk/villagereach/MainActivity.java
@@ -2,9 +2,11 @@ package com.lawk.villagereach;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,29 +15,27 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "myTracker";
     private static final int RESULT_ID = 1;
     public static final String MESSAGE_ID = "loginInfo";
+    private ArrayList<Login> loginArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginArrayList = new ArrayList<>();
     }
 
-    public void onClick(View button) {
 
-        Log.i(TAG, "Main activity button " + button.getId() + " clicked");
+   public void onClick(View button) {
 
-        EditText username = findViewById(R.id.username);
-        String myUserName = username.getText().toString();
-        //username.getText().clear();
+       EditText username = findViewById(R.id.username);
+       String myUserName = username.getText().toString();
 
-        EditText password = findViewById(R.id.password);
-        String myPassword = password.getText().toString();
-        //password.getText().clear();
+       EditText password = findViewById(R.id.password);
+       String myPassword = password.getText().toString();
 
-        String mymessage = myUserName + myPassword;
+       loginArrayList.add(new Login(myUserName, myPassword));
 
-        Intent intent = new Intent(this, DeliveryActivity.class);
-        intent.putExtra(MESSAGE_ID, mymessage);
-        startActivityForResult(intent, RESULT_ID);
-    }
+   }
+
+
 }
