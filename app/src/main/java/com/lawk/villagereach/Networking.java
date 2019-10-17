26af16caf8be89dd926fd435311c2c2c4008f09d
towa-hi <yamyamasunday@gmail.com>
2019-10-16 21:41:10 -0700

    fixed previous commit

--- app/src/main/java/com/lawk/villagereach/Networking.java
+++ app/src/main/java/com/lawk/villagereach/Networking.java
@@ -1,4 +1,15 @@
 package com.lawk.villagereach;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+
 public class Networking {
+    public boolean isConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
