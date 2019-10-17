b4e802e808dc1bdce490399743f109a4013f8812
towa-hi <yamyamasunday@gmail.com>
2019-10-16 21:12:12 -0700

    added credentials class

---
+++ app/src/main/java/com/lawk/villagereach/Credentials.java
@@ -0,0 +1,10 @@
+package com.lawk.villagereach;
+
+public class Credentials {
+    String username;
+    String password;
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
