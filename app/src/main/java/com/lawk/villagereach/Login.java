5e993c7cbb3f96753ecbe98764310f84d8847d00
towa-hi <yamyamasunday@gmail.com>
2019-10-19 18:27:20 -0700

    fleshed out sync class

--- app/src/main/java/com/lawk/villagereach/Login.java
+++ app/src/main/java/com/lawk/villagereach/Login.java
@@ -10,5 +10,6 @@ public class Login {
     public static void login(String username, String password, Context context) {
         Credentials creds = new Credentials(username, password);
         Auth.authenticate(creds, context);
+        Sync.sync(context);
     }
 }
