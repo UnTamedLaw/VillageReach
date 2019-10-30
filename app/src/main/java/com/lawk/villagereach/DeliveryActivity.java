46683fe7372176fc2265ca8a86d2929d0234ca90
Abby Abraha <Tyeser@gmail.com>
2019-10-29 17:04:15 -0700

    Added Toast to Delivery and Sync Buttons

--- app/src/main/java/com/lawk/villagereach/DeliveryActivity.java
+++ app/src/main/java/com/lawk/villagereach/DeliveryActivity.java
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 
@@ -16,7 +19,7 @@ import java.util.ArrayList;
 public class DeliveryActivity extends AppCompatActivity {
 
     private static final String TAG = "Anu";
-    private static  String RESULT = "DeliveryResponse";
+    private static String RESULT = "DeliveryResponse";
     private RecyclerView recyclerView;
     private OrderRecyclerAdapter orderRecyclerAdapter;
     private ArrayList<Order> orderModelArrayList;
@@ -36,9 +39,25 @@ public class DeliveryActivity extends AppCompatActivity {
 
         orderRecyclerAdapter = new OrderRecyclerAdapter(DeliveryActivity.this, orderModelArrayList);
         recyclerView.setAdapter(orderRecyclerAdapter);
+        Button getDeliveries = findViewById(R.id.delivery);
+        Button getSync = findViewById(R.id.sync);
 
     }
-   public void formActivity(View view) {
+
+    public void showDelivery(View view) {
+        Toast delivery = Toast.makeText(getApplicationContext(), "No Network Connection! Deliveries Can't Be Complete!",
+                Toast.LENGTH_SHORT);
+        delivery.show();
+    }
+
+    public void showSync(View view) {
+        Toast sync = Toast.makeText(getApplicationContext(), "No Network Connection! Sync Can't Be Completed!",
+                Toast.LENGTH_SHORT);
+        sync.show();
+
+    }
+
+    public void formActivity(View view) {
         Intent intent = new Intent(DeliveryActivity.this, FormActivity.class);
         startActivity(intent);
     }
