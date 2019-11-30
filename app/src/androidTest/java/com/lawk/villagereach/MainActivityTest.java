6c55999a411119d321662c840e6e78167f3b42bd
towa-hi <yamyamasunday@gmail.com>
2019-11-30 15:17:36 -0800

    main activity test

--- app/src/androidTest/java/com/lawk/villagereach/MainActivityTest.java
+++ app/src/androidTest/java/com/lawk/villagereach/MainActivityTest.java
@@ -2,12 +2,33 @@ package com.lawk.villagereach;
 
 import android.content.Context;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
+    @Test
+    public void canLogin() throws InterruptedException {
+        Intents.init();
+        onView(withId(R.id.username)).perform(replaceText("administrator"));
+        onView(withId(R.id.password)).perform(replaceText("password"));
+        onView(withId(R.id.loginButton)).perform(click());
+        Thread.sleep(10000);
+        intended(hasComponent(DeliveryActivity.class.getName()));
+
+    }
 }
