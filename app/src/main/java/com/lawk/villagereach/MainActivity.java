6461faad185938cfbba20cc13e8fa4e8e5b2ca06
Kevin Law <kevin@Thaumaturge-Pro.local>
2019-12-03 03:16:58 -0800

    deleted unnecessary code

--- app/src/main/java/com/lawk/villagereach/MainActivity.java
+++ app/src/main/java/com/lawk/villagereach/MainActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.NoConnectionError;
 
 
 public class MainActivity extends AppCompatActivity {
+    
 
     private static final String TAG = "myTracker";
     private static final int RESULT_ID = 1;
